5f0443dce7ed6fca4b212f05305bc9a566e8689b
Quyen Doan <qdoan1651@gmail.com>
2019-09-19 09:20:09 -0800

    Added code for interfaces from Mike McMillan

---
+++ src/oop/interfaces/shape/Shape.java
@@ -0,0 +1,15 @@
+/* 
+ * Beginners Java Programming by Mike McMillan
+ * Chapter 14: Polymorphism
+ * Lesson 5: Working With Interfaces
+ */
+package oop.interfaces.shape;
+
+public interface Shape {
+   public int getX();
+   public int getY();
+   public void setX(int xcor);
+   public void setY(int ycor);
+   public void draw();
+}
+
